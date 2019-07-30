84ddfa1ce3bfe2615b0f04ec5d74467f8102ecec
Awais Chishti <chishtiawais511@gmail.com>
2019-07-30 13:18:20 +0500

    Re-add module

--- configure.sh
+++ configure.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd backend/transmission/ && git checkout label-search && mkdir build && git submodule update --init && cd build && cmake .. && make
+cd backend/transmission && git checkout label-search && mkdir build && git submodule update --init && cd build && cmake .. && make
